45e7f44ea576e45de82143aea24f7bf3cc1978f7
Littline <sujiuzheng@gmail.com>
2026-06-09 22:46:22 +0800

    modify seo info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "http://127.0.0.1:4321",
+  site: "https://www.zh.xiangqi.com/",
   output: "static",
   trailingSlash: "always"
 });
