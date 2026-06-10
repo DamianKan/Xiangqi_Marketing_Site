22a5f8e5af6ed25765a3295ed5d16f8b95f9b3a8
Littline <sujiuzheng@gmail.com>
2026-06-10 13:33:09 +0800

    add json-ld

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,7 +1,9 @@
 import site from "../data/site.json";
 
 export function GET() {
-  const body = `User-agent: *\nAllow: /\n\nSitemap: ${site.siteUrl}/sitemap.xml\n`;
+  const sitemapUrl = new URL("/sitemap.xml", site.siteUrl).toString();
+  const llmsUrl = new URL("/llms.txt", site.siteUrl).toString();
+  const body = `User-agent: *\nAllow: /\n\nSitemap: ${sitemapUrl}\nLLMS: ${llmsUrl}\n`;
 
   return new Response(body, {
     headers: {
